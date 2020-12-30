590b535a4e43674177e224cdca850e47c18edab1
mb2081 <alison.graham@gdit.com>
2020-12-30 14:06:57 -0700

    Added a Redux example in Playground folder

---
+++ src/playground/redux-101.js
@@ -0,0 +1,70 @@
+import { createStore } from 'redux';
+
+const store = createStore((state = { count: 0}, action ) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+            return {
+                count: state.count + incrementBy
+            }; 
+        case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
+            return {
+                count: state.count - decrementBy
+            };
+        case 'SET':
+            return {
+                count: action.count
+            }
+        case 'RESET':
+            return {
+                count: 0
+            };
+        default:
+            return state;
+    }
+});
+
+//Gets called every time the store changes
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+})
+
+
+//increment the count
+store.dispatch({
+    type: 'INCREMENT',
+    incrementBy: 5
+});
+
+//when you are done tracking the state
+//unsubscribe();
+
+//increment the count
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+//reset the count
+store.dispatch({
+    type: 'RESET'
+});
+
+//decrement the count
+store.dispatch({
+    type: 'DECREMENT',
+});
+
+//decrement the count
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 10
+});
+
+store.dispatch({
+    type: 'SET',
+    count: 101
+});
+
+
+
